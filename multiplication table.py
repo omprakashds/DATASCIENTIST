8e44018bafbcd380b88a2984313077b40b8e0328
omprakash845413 <153392569+omprakash845413@users.noreply.github.com>
2024-02-23 14:36:46 +0530

    Update multiplication table.py

--- multiplication table.py
+++ multiplication table.py
@@ -1,4 +1,23 @@
-for i in range(1, 12 + 1): # Formatting row number
-      for j in range(1, 12 + 1):
-          print("{:4d}".format(i * j),end="") # Formatting column number  
-      print()
+def create_multiplication_table(size):
+  """Creates a neatly formatted multiplication table up to the given size."""
+
+  # Header Row
+  print("   ", end="")
+  for i in range(1, size + 1):
+    print("{:4d}".format(i), end="")  # Formatting column headers
+  print()
+
+  # Separator Line
+  print("   ", "-" * (size * 4 + 1))
+
+  # Table Content
+  for i in range(1, size + 1):
+    print("{:2d} |".format(i), end="")  # Formatting row number
+    for j in range(1, size + 1):
+      print("{:4d}".format(i * j), end="")
+    print()
+
+
+# Example usage
+size = 12
+create_multiplication_table(size)
